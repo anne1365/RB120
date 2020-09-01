f42ca9b3659e2457f6282a25d236cfd4ddb913d2
Anne <anne1365@outlook.com>
2020-08-31 19:11:05 -0700

    Practice

--- StudyGroup/study_group_083120.rb
+++ StudyGroup/study_group_083120.rb
@@ -142,3 +142,23 @@ end
 
 bob = Persons.new
 bob.who_am_i
+
+
+class Person 
+  include Comparable 
+    attr_accessor :name, :age 
+
+    def initialize(name, age) 
+        @name = name 
+        @age = age 
+    end
+
+    def <=>(other_person) 
+        age <=> other_person.age 
+    end
+end
+
+bob = Person.new("Bob", 49) 
+kim = Person.new("Kim", 33)
+
+puts bob > kim 
