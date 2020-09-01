b965a0f16bf7571ac18d660979d01a527b9e8a7f
Anne <anne1365@outlook.com>
2020-09-01 13:04:58 -0700

    Practice

---
+++ L1-OO_Readings/Playing_with_classes.rb
@@ -0,0 +1,42 @@
+class Cat
+  def initialize(name)
+    @name = name
+    puts name
+  end
+
+  def name        # In ruby getters and setters are named after the        
+    @name         # instance variables they work with, rather than 'get_name'
+  end             # or 'set_name'
+
+  def name=(name)
+    @name = name
+  end
+end
+
+
+sherlock = Cat.new("Sherlock")
+puts sherlock.name
+
+# two ways to invoke setter method - first is syntactical sugar
+puts sherlock.name = "Sherlock my boiii"
+puts sherlock.name=("Sherlock")
+
+# --------------------------------------------------------------------------------------------------
+class GoodDog
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def speak
+    "#{@name} says arf!"
+  end
+end
+
+sparky = GoodDog.new("Sparky")
+puts sparky.speak
+puts sparky.name
+
+sparky.name = "Spartacus"
+puts sparky.name
