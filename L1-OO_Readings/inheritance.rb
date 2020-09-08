1af31c52baf3e501743b09c10a5d47852d9c7096
Anne <anne1365@outlook.com>
2020-09-08 00:48:56 -0700

    oop

--- L1-OO_Readings/inheritance.rb
+++ L1-OO_Readings/inheritance.rb
@@ -1,1 +1,39 @@
+class Animal
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+class GoodDog < Animal
+  attr_accessor :color
+  
+  def initialize(color)
+    super
+    @color = color
+  end
+
+  def info
+    puts "#{self.name} #{self.color}"
+  end
+end
+
+
+class BadDog < Animal 
+  def initialize(age, name)
+    super(name)
+    @age = age
+  end
+end
+
+
+
+sparky = GoodDog.new("brown")
+sparky.info # => both name and color = brown
+
+p BadDog.new(2, "bear") # => name = bear & age = 2
+# this is because we sent an argument to superclass
+
 
