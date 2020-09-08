ec67de4a5a814d6690a3d3d87dd5e2302812e2d2
Anne <anne1365@outlook.com>
2020-09-08 02:11:26 -0700

    OOP

--- L1-OO_Readings/inheritance.rb
+++ L1-OO_Readings/inheritance.rb
@@ -12,6 +12,11 @@ class GoodDog < Animal
   
   def initialize(color)
     super
+  
+# if we call super like this, we're calling super with no arguments, but
+# the initialize method here takes one argument, super automatically forwards
+# the arguments that were passed to the method from which super is called
+
     @color = color
   end
 
@@ -24,16 +29,35 @@ end
 class BadDog < Animal 
   def initialize(age, name)
     super(name)
+
+# if we call super like this, we pass the argument passed to super to the
+# superclass
+
     @age = age
+  end
+end
+
+
+class Bear < Animal
+  def initialize(color)
+    super()
+
+# if we call super like this - with no parentheses - it will call the 
+# method in the superclass with no arguments - this is the safest way to
+# call a method in our superclass that takes no arguments
+
+    @color = color
   end
 end
 
 
 
 sparky = GoodDog.new("brown")
-sparky.info # => both name and color = brown
+p sparky.info # => both name and color = brown
 
 p BadDog.new(2, "bear") # => name = bear & age = 2
 # this is because we sent an argument to superclass
+
+bear = Bear.new("black")
 
 
--- L1-OO_Readings/tempCodeRunnerFile.rb
+++ L1-OO_Readings/tempCodeRunnerFile.rb
@@ -1,3 +1,6 @@
-  def to_s
-    "This dog's name is #{name} and it is #{age} years old in dog years."
-  end
+
+# sparky = GoodDog.new("brown")
+# p sparky.info # => both name and color = brown
+
+# p BadDog.new(2, "bear") # => name = bear & age = 2
+# # this is because we sent an argument to superclass
