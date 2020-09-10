6806c9ad9598c1135e2787ead22cfdaf7760e132
Anne Jones <anne1365@outlook.com>
2020-09-10 10:18:56 -0700

    Practice

--- L2-OO_Programming/lecture_inheritance.rb
+++ L2-OO_Programming/lecture_inheritance.rb
@@ -1,10 +1,6 @@
-class Dog
+class Pet
   def speak
-    'bark!'
-  end
-
-  def swim
-    'swimming'
+    'Animal noise!'
   end
 
   def run
@@ -14,17 +10,38 @@ class Dog
   def jump
     'jumping!'
   end
+end
+
+
+class Cat < Animal
+  def speak
+    'Meow!'
+  end
+end
+
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming'
+  end
 
   def fetch
     'fetching!'
   end
 end
 
+
 class Bulldog < Dog
   def swim
     "can't swim!"
   end
 end
+
+
 
 teddy = Bulldog.new
 puts teddy.speak
