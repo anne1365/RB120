b4fedf0fdde810703576c3c0905fa6bb248c90fc
Anne Jones <anne1365@outlook.com>
2020-09-11 17:40:31 -0700

    Polishing & adding features for oo_rps

--- L2-OO_Programming/oo_rps.rb
+++ L2-OO_Programming/oo_rps.rb
@@ -1,26 +1,28 @@
+class Score
+  attr_accessor :score
+
+  def initialize
+    @score = 0
+  end
+
+  def increment
+    @score += 1
+  end
+end
+
 class Move
-  VALUES = ['rock', 'paper', 'scissors']
+  VALUES = { 'rock' => ['scissors', 'lizard'],
+             'paper' => ['spock', 'rock'],
+             'scissors' => ['paper', 'lizard'],
+             'lizard' => ['spock', 'paper'],
+             'spock' => ['rock', 'scissors'] }
 
   def initialize(value)
     @value = value
   end
 
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def scissors?
-    @value == 'scissors'
-  end
-
   def >(other_move)
-    (rock? && other_move.scissors?) ||
-      (paper? && other_move.rock?) ||
-      (scissors? && other_move.paper?)
+    VALUES[@value].include?(other_move.to_s)
   end
 
   def to_s
@@ -29,10 +31,11 @@ class Move
 end
 
 class Player
-  attr_accessor :move, :name
+  attr_accessor :move, :name, :all_moves
 
   def initialize
     set_name
+    @all_moves = []
   end
 end
 
@@ -43,7 +46,7 @@ class Human < Player
       puts "What's your name?"
       n = gets.chomp
       break unless n.empty?
-      puts "Sorry, must enter a value!"
+      puts("Error: Invalid entry!")
     end
 
     self.name = n
@@ -52,12 +55,13 @@ class Human < Player
   def choose
     choice = ''
     loop do
-      puts "Please choose rock, paper, or scissors: "
+      print "\nChoose rock, paper, scissors, lizard, or spock: "
       choice = gets.chomp
       break if Move::VALUES.include?(choice)
-      puts "Sorry, invalid choice."
+      puts("Error: That's not a valid choice!")
     end
     self.move = Move.new(choice)
+    all_moves << move.to_s
   end
 end
 
@@ -67,66 +71,143 @@ class Computer < Player
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    self.move = Move.new(Move::VALUES.keys.sample)
+    all_moves << move.to_s
   end
 end
 
 class RPSGame
-  attr_accessor :human, :computer
+  attr_accessor :human, :computer, :human_pts, :computer_pts
 
   def initialize
+    system('clear') || system('cls')
+    display_welcome_message
     @human = Human.new
     @computer = Computer.new
+    @human_pts = Score.new
+    @computer_pts = Score.new
+  end
+
+  private
+
+  def display_divider
+    puts "--------------------------------------------------------"
+  end
+
+  def new_round
+    @human_pts = Score.new
+    @computer_pts = Score.new
+    human.all_moves.clear
+    computer.all_moves.clear
+    clear_screen
   end
 
   def display_welcome_message
-    puts "Welcome to Rock, Paper, Scissors!"
+    puts("--------------------------------------------------------")
+    puts("    Welcome to Rock, Paper, Scissors, Lizard, Spock!    ")
+    puts('--------------------------------------------------------')
+    puts "   >>> THE FIRST PLAYER TO 5 WILL RULE THE GALAXY <<<\n\n"
+  end
+
+  def make_choices
+    human.choose
+    computer.choose
   end
 
   def display_moves
+    display_divider
     puts "#{human.name} chose #{human.move}."
-    puts "#{computer.name} chose #{computer.move}."
+    puts "#{computer.name} chose #{computer.move}.\n\n"
   end
 
   def display_winner
     if human.move > computer.move
-      puts "#{human.name} won!"
+      puts "YOU WON! :D"
+      human_pts.increment
     elsif computer.move > human.move
-      puts "#{computer.name} won!"
+      puts "COMPUTER WON! :("
+      computer_pts.increment
     else
-      puts "It's a tie!"
+      puts "IT'S A TIE!"
     end
   end
 
+  def display_scoreboard
+    puts "\nYou have #{human_pts.score} points."
+    puts "#{computer.name} has #{computer_pts.score} points."
+  end
+
+  def display_round_outcome
+    display_moves
+    display_winner
+    display_scoreboard
+  end
+
+  def final_winner?
+    (computer_pts.score >= 3) || (human_pts.score >= 3)
+  end
+
+  def display_final_winner
+    puts
+    display_divider
+
+    if human_pts.score > computer_pts.score
+      puts "HUMANITY PREVAILS - YOU'VE WON THE TOURNAMENT!"
+    elsif computer_pts.score > human_pts.score
+      puts "#{computer.name.upcase} HAS WON THE TOURNAMENT!"
+    else
+      puts "NO VICTORS TODAY - IT'S A TIE!"
+    end
+
+    display_divider
+  end
+
+  def display_all_moves
+    puts "YOUR MOVES:"
+    human.all_moves.each_with_index { |move, n| puts "#{n + 1}. #{move}" }
+    puts "\n#{computer.name.upcase}'S MOVES:"
+    computer.all_moves.each_with_index { |move, n| puts "#{n + 1}. #{move}" }
+    display_divider
+  end
+
   def display_goodbye_message
-    puts "Thanks for playing!"
+    puts "\nThanks for playing!"
+    clear_screen
+  end
+
+  def clear_screen
+    puts "Hit any key to continue."
+    gets.chomp
+    system('clear') || system('cls')
   end
 
   def play_again?
     answer = ''
     loop do
-      puts "Would you like to play again? (y/n)"
+      print "\nWould you like to play again? (y/n): "
       answer = gets.chomp
       break if ['y', 'n'].include?(answer.downcase)
-      puts "Sorry, invalid choice."
+      puts("Error: That's not a valid choice!")
     end
-
     answer.downcase == 'y'
   end
 
-  def play
-    display_welcome_message
+  public
 
+  def play
     loop do
-      human.choose # we want human input
-      computer.choose # we want random logic - how to differentiate?
-      display_moves
-      display_winner
-      break unless play_again? == true
+      loop do
+        make_choices
+        display_round_outcome
+        break display_final_winner if final_winner?
+      end
+
+      display_all_moves
+      play_again? == true ? new_round : break
     end
 
     display_goodbye_message
   end
 end
 
-RPSGame.new.play # how we might kick off the gameplay
+RPSGame.new.play # how we might kick off the gameplay rock
--- L2-OO_Programming/tempCodeRunnerFile.rb
+++ L2-OO_Programming/tempCodeRunnerFile.rb
@@ -1,2 +1,6 @@
-person1.phone_number = 498576
-# puts person1.phone_number
+  # p other_move
+    # p other_move.rock?
+    # p other_move.paper?
+    # p other_move.scissors?
+    # p other_move.lizard?
+    # p other_move.spock?
